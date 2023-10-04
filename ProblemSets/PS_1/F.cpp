bdfe83a1ddd49122e6c92301a34ea1dc002ef32e
Vincent Yu <sydney.vincent.yu@gmail.com>
2023-10-05 00:49:15 +1100

    PS1 F started

--- ProblemSets/PS_1/F.cpp
+++ ProblemSets/PS_1/F.cpp
@@ -1,19 +1,24 @@
 #include <iostream>
 using namespace std;
- 
+
+typedef long long ll;
+
 const int N = 1010;
-int arr[N];
+ll arr[N];
 
 // An optimized version of Bubble Sort
 void bubbleSort(int n)
 {
-    int i, j;
+    string str;
+    ll invCounter = 0;
+    ll i, j;
     bool swapped;
     for (i = 0; i < n - 1; i++) {
         swapped = false;
         for (j = 0; j < n - i - 1; j++) {
             if (arr[j] > arr[j + 1]) {
-                cout << j << ' ' << j+1 << '\n';
+                str +=  to_string(j+1) + ' ' + to_string(j+2) + '\n';
+                invCounter ++;
                 swap(arr[j], arr[j + 1]);
                 swapped = true;
             }
@@ -21,14 +26,17 @@ void bubbleSort(int n)
         if (swapped == false)
             break;
     }
+
+    cout << invCounter << '\n';
+    cout << str << '\n';
 }
  
 // Driver program to test above functions
 int main()
 {
-    int n;
+    ll n;
     cin >> n;
-    for(int i = 0; i < n; i ++){
+    for(ll i = 0; i < n; i ++){
         cin >> arr[i];
     }
     bubbleSort(n);
