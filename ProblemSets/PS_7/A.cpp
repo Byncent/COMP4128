f293e4285f5512a7cecdbb863df63605be574d39
Vincent Yu <Sydney.Vincent.Yu@gmail.com>
2023-11-17 18:28:00 +1100

    PS7 A completed

--- ProblemSets/PS_7/A.cpp
+++ ProblemSets/PS_7/A.cpp
@@ -14,97 +14,102 @@ using namespace std;
 typedef long long ll;
 const ll INF = 1e15;
 struct cell{
-    ll v = 0;
+    ll vin = 0;
+    ll vout = 0;
     ll c = 0;
 };
+/*
+ * dinic 1
+ */
 
 struct FlowNetwork {
-    ll n;
-    vector<vector<ll>> adjMat, adjList;
+    int n;
+    vector<vector<ll> > adjMat, adjList;
     // level[v] stores dist from s to v
     // uptochild[v] stores first non-useless child.
-    vector<ll> level, uptochild;
+    vector<int> level, uptochild;
 
-    FlowNetwork(ll _n) : n(_n) {
+    FlowNetwork (int _n): n(_n) {
         // adjacency matrix is zero-initialised
         adjMat.resize(n);
-        for (ll i = 0; i < n; i++)
+        for (int i = 0; i < n; i++)
             adjMat[i].resize(n);
         adjList.resize(n);
         level.resize(n);
         uptochild.resize(n);
     }
 
-    void add_edge(ll u, ll v, ll c) {
+    void add_edge (int u, int v, ll c) {
         // add to any existing edge without overwriting
-        adjMat[u][v] += c;
+        adjMat[u][v] += c;        
         adjList[u].push_back(v);
         adjList[v].push_back(u);
     }
 
-    void flow_edge(ll u, ll v, ll c) {
+    void flow_edge (int u, int v, ll c) {
         adjMat[u][v] -= c;
         adjMat[v][u] += c;
     }
+
+/*
+ * dinic 2
+ */
+
     // constructs the BFS tree and returns whether the sink is still reachable
-    bool bfs(ll s, ll t) {
+    bool bfs(int s, int t) {
         fill(level.begin(), level.end(), -1);
-        queue<ll> q;
+        queue<int> q;
         q.push(s);
         level[s] = 0;
-
         while (!q.empty()) {
-            ll u = q.front();
-            q.pop();
+            int u = q.front(); q.pop();
             uptochild[u] = 0; // reset uptochild
-
-            for (ll v : adjList[u]) {
+            for (int v : adjList[u])
                 if (adjMat[u][v] > 0) {
                     if (level[v] != -1) // already seen
                         continue;
                     level[v] = level[u] + 1;
                     q.push(v);
                 }
-            }
         }
-
         return level[t] != -1;
     }
 
+/*
+ * dinic 3
+ */
+
     // finds an augmenting path with up to f flow.
-    ll augment(ll u, ll t, ll f) {
-        if (u == t)
-            return f; // base case.
-
-        // note the reference here! we increment uptochild[u], i.e. walk through
-        // u's neighbours until we find a child that we can flow through
-        for (ll &i = uptochild[u]; i < adjList[u].size(); i++) {
-            ll v = adjList[u][i];
-
+    ll augment(int u, int t, ll f) {
+        if (u == t) return f; // base case.
+        // note the reference here! we increment uptochild[u], i.e. walk through u's neighbours
+        // until we find a child that we can flow through
+        for (int &i = uptochild[u]; i < adjList[u].size(); i++) {
+            int v = adjList[u][i];
             if (adjMat[u][v] > 0) {
                 // ignore edges not in the BFS tree.
-                if (level[v] != level[u] + 1)
-                    continue;
-
+                if (level[v] != level[u] + 1) continue;
                 // revised flow is constrained also by this edge
-                ll rf = augment(v, t, min(f, adjMat[u][v]));
-
+                ll rf = augment(v,t,min(f, adjMat[u][v]));
                 // found a child we can flow through!
                 if (rf > 0) {
-                    flow_edge(u, v, rf);
+                    flow_edge(u,v,rf);
                     return rf;
                 }
             }
         }
-
         level[u] = -1;
         return 0;
     }
 
-    ll dinic(ll s, ll t) {
+/*
+ * dinic 4
+ */
+
+    ll dinic(int s, int t) {
         ll res = 0;
-        while (bfs(s, t))
-            for (ll x = augment(s, t, INF); x; x = augment(s, t, INF))
+        while (bfs(s,t))
+            for (ll x = augment(s,t,INF); x; x = augment(s,t,INF))
                 res += x;
         return res;
     }
@@ -116,7 +121,7 @@ cell graph[50][50];
 int main(){
     ll n, m, c;
     cin >> n >> m >> c;
-    FlowNetwork network(n * m + 2);
+    FlowNetwork network(2*(n * m + 1));
 
     // node index
     ll cnt = 0;
@@ -125,9 +130,7 @@ int main(){
         for(ll j = 0; j < n; j ++){
             char chr;
             cin >> chr;
-
-            // assign node index
-            graph[i][j].v = cnt;
+            graph[i][j].vin = cnt;
 
             // if node is a dot, then capacity of edge to this node is infinity (0 maps to INF in map)
             if(chr == '.'){
@@ -144,10 +147,14 @@ int main(){
                 graph[i][j].c = chr - 'a' + 1;
             }
             // cout << "cnt = " << cnt << '\n';
+            // assign node index
+            cnt ++;
+            graph[i][j].vout = cnt;
             cnt ++;
         }
     }
     t = cnt;
+    // cout <<"t = " << t << '\n';
 
     // map 0 to infinity    
     weights[0] = INF;
@@ -157,27 +164,28 @@ int main(){
     for(ll i = 0; i < m; i ++){
         for(ll j = 0; j <n; j ++){
 
+            network.add_edge(graph[i][j].vin, graph[i][j].vout, weights[graph[i][j].c]);
+
             if(i - 1 < 0 || j - 1 < 0 || i + 1 >= m || j + 1 >= n){
                 // cout << "edge added: [" << i << "][" << j << "] - " <<"sink = " << INF <<"\n";
-                network.add_edge(graph[i][j].v, t, INF);
+                network.add_edge(graph[i][j].vout, t, INF);
             }
             if(i-1 >=0){
                 // cout << "edge added: [" << i << "][" << j << "] - " <<"[" << i-1 << "][" << j << "] = " << weights[graph[i-1][j].c]<< "\n";
-                network.add_edge(graph[i][j].v, graph[i-1][j].v, weights[graph[i-1][j].c]);
+                network.add_edge(graph[i][j].vout, graph[i-1][j].vin, INF);
             }
             if(j-1 >=0){
                 // cout << "edge added: [" << i << "][" << j << "] - " <<"[" << i << "][" << j-1 << "] = " << weights[graph[i][j-1].c] << "\n";
-                network.add_edge(graph[i][j].v, graph[i][j-1].v, weights[graph[i][j-1].c]);
+                network.add_edge(graph[i][j].vout, graph[i][j-1].vin, INF);
             }
             if(i+1 <m){
                 // cout << "edge added: [" << i << "][" << j << "] - " <<"[" << i+1 << "][" << j << "] = " << weights[graph[i+1][j].c] << "\n";
-                network.add_edge(graph[i][j].v, graph[i+1][j].v, weights[graph[i+1][j].c]);
+                network.add_edge(graph[i][j].vout, graph[i+1][j].vin, INF);
             }
             if(j+1 <n){
                 // cout << "edge added: [" << i << "][" << j << "] - " <<"[" << i << "][" << j+1 << "] = " <<  weights[graph[i][j+1].c] << "\n";
-                network.add_edge(graph[i][j].v, graph[i][j+1].v, weights[graph[i][j+1].c]);
+                network.add_edge(graph[i][j].vout, graph[i][j+1].vin, INF);
             }
-            
         }
     }
     ll res = network.dinic(s, t);
